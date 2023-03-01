09dd70e2a37439abfc4edff4dfe05972308d4b91
johnsaigle <nothanks>
2023-03-01 15:19:05 -0500

    init

---
+++ README.md
@@ -0,0 +1,9 @@
+# Go Fuzz Harness Template
+
+A template to help with writing a harness for fuzzing using Go's built-in fuzz testing.
+
+## Usage
+
+```bash
+go test -fuzz=Fuzz
+```
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+// From: "Tutorial: Getting started with fuzzing - The Go Programming Language"
+// https://go.dev/doc/tutorial/fuzz
+func Reverse(s string) string {
+	b := []byte(s)
+	for i, j := 0, len(b)-1; i < len(b)/2; i, j = i+1, j-1 {
+		b[i], b[j] = b[j], b[i]
+	}
+	return string(b)
+}
